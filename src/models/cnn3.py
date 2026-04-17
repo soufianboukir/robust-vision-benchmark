dfab4b067cc50b4cac838c00f20f623e7229de07
soufian <soufianeboukir0@gmail.com>
2026-04-17 22:01:22 +0100

    CNN3: fix bugs

--- src/models/cnn3.py
+++ src/models/cnn3.py
@@ -6,24 +6,24 @@ class CNN3(nn.Module):
         self.net = nn.Sequential(
             # conv block 1
             nn.Conv2d(3, 32, kernel_size=3, padding=1), # producing 32 feature maps, 3 for nm of channels
-            nn.Relu(),
+            nn.ReLU(),
             nn.MaxPool2d(2,2),
 
             # conv block 2
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.Relu(),
+            nn.ReLU(),
             nn.MaxPool2d(2,2),
 
             # conv block 3
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.Relu(),
+            nn.ReLU(),
             nn.MaxPool2d(2,2),
 
             # fully connected layers
             nn.Flatten(),
-            nn.Conv2d(128 * 4 * 4, 256),
-            nn.Relu(),
-            nn.Conv2d(256,10),
+            nn.Linear(128 * 4 * 4, 256),
+            nn.ReLU(),
+            nn.Linear(256,10),
         )
 
     def forware(self, x):
