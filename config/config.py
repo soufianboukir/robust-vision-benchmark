8543a489c386a07c87ef92d742f36de8b99213e4
Soufian <soufianeboukir0@gmail.com>
2026-04-27 09:22:13 +0000

    add resnet18 results

--- config/config.py
+++ config/config.py
@@ -2,13 +2,13 @@ import os
 
 class Config:
 
-    batch_size = 64
-    learning_rate = 1e-3
-    epochs = 30
+    batch_size = 32
+    learning_rate = 8e-4
+    epochs = 40
 
     device = "cuda"
 
-    model_type = "resnet18"
+    model_type = "resnet50"
 
     data_path = "./data"
     save_dir = os.path.join(os.getcwd(), "saved_models")
